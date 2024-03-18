5231e34930c7ef9aab0e48c6deaae554877ab952
rlodhiya <67738483+rlodhiya2502@users.noreply.github.com>
2024-03-18 18:28:30 +0000

    sqlite implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,21 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { WhatsAppModule } from './whats-app/whats-app.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
 
 @Module({
   imports: [
     UsersModule,
     AuthModule,
     WhatsAppModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db/sql',
+      synchronize: true,
+      entities: [User],
+    }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
