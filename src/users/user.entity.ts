d7ef1d47ecb545214f16ca7e4f0bdad78709a4f0
rlodhiya <67738483+rlodhiya2502@users.noreply.github.com>
2024-03-18 18:32:36 +0000

    sqlite implemented (#1)

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,5 +1,31 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+ 
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('Users')
 export class User {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ name: 'name', length: 70, nullable: false })
   name: string;
+
+  @Column({ name: 'mobile', length: 12, nullable: false })
   mobile: string;
+
+  @Column({ name: 'logged', nullable: true })
+  logged?: boolean;
+
+  @Column({ nullable: true })
+  @CreateDateColumn()
+  created?: string; // date
+  
+  @Column({ nullable: true })
+  @UpdateDateColumn()
+  updated?: string; // date
 }
